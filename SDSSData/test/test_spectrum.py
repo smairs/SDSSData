9259667587eafb34fdc602af0f1ef82ded598b2f
smairs <s.mairs@eaobservatory.org>
2019-09-13 11:37:34 -1000

    Added testing feartures for other weather keywords

--- SDSSData/test/test_spectrum.py
+++ SDSSData/test/test_spectrum.py
@@ -14,6 +14,19 @@ def test_pressure():
     a = Spectrum(example_fits)
     assert a.hd.get('PRESSURE') != None, "PRESSURE keyword doesn't exist!"
 
+def test_airtemp():
+    a = Spectrum(example_fits)
+    assert a.hd.get('AIRTEMP') != None, "AIRTEMP keyword doesn't exist!"
 
+def test_humidity():
+    a = Spectrum(example_fits)
+    assert a.hd.get('HUMIDITY') != None, "HUMIDITY keyword doesn't exist!"
 
+def test_winds():
+    a = Spectrum(example_fits)
+    assert a.hd.get('WINDS') != None, "WINDS keyword doesn't exist!"
+
+def test_windd():
+    a = Spectrum(example_fits)
+    assert a.hd.get('WINDD') != None, "WINDD keyword doesn't exist!"
 
