b8e448428baa706d609eac86e639b9212cd7b299
smairs <s.mairs@eaobservatory.org>
2019-09-13 13:09:51 -1000

    Reorganised spectrum.py into logical sections, commented on it, and replaced numpy where commands with cleaner, more readable alternatives

--- SDSSData/spectrum.py
+++ SDSSData/spectrum.py
@@ -5,16 +5,27 @@ from scipy.optimize import curve_fit
 
 class Spectrum():
     def __init__(self,datafile):
+        
+        # Keep the init method light,
+        # could define a deparate "open"
+        # method - but if we are loading in
+        # a spectrum, we will probably
+        # always want all of this information
+        # immediately handy
+
         self._datafile = datafile
 
-        # get flux information and harvest the header
+        # Extract primary data and header
+        # There is no data in hdu_list[0].data
         hdu_list      = fits.open(self._datafile)
         data          = hdu_list[1].data
         hd            = hdu_list[0].header
+
+        # Get flux and wavelength information
         self.loglam   = data['loglam']
         self.flux     = data['flux']        
 
-        # extract header info
+        # Make full header available and extract other info
         self.hd       = hd
         self.ra       = hd['RA']
         self.dec      = hd['DEC']
@@ -23,23 +34,25 @@ class Spectrum():
         self.plate    = hd['NAME']
         self.survey   = hd['PLATETYP']
         self.exptime  = hd['EXPTIME']
-   
-    # By: smairs
-    def weather(self):
-        print('\nSummary of the Weather:')
-        print('-----------------------\n')
-        print(f"Bolometric Pressure = {self.hd['PRESSURE']}")
-        print(f"Wind Speed          = {self.hd['WINDS']}")
-        print(f"Wind Direction      = {self.hd['WINDD']}")
-        print(f"Air Temperature     = {self.hd['AIRTEMP']}")
-        print(f"Humidity            = {self.hd['HUMIDITY']}\n")
+        
+        # Weather Keywords:
+        self.pressure = hd['PRESSURE']
+        self.windspeed= hd['WINDS']
+        self.winddir  = hd['WINDD']
+        self.airtemp  = hd['AIRTEMP']
+        self.humidity = hd['HUMIDITY']
+
+
+###########
+# Quick ways to grab the data and view it
+###########
 
     # By: smairs
     def get_spectrum(self):
         '''
         Get the wavelength and flux data of an SDSS spectrum
     
-        loglam, flux = get_spectrum()
+        loglam, flux = Spectrum.get_spectrum()
         '''
         return(self.loglam,self.flux)
 
@@ -47,100 +60,21 @@ class Spectrum():
     def showme(self):
         '''
         Quick, non-customisable view of the spectrum
+
+        Spectrum.showme()
         '''
         plt.plot(self.loglam,self.flux)
         plt.ylabel('Flux')
         plt.xlabel('Loglam')
         plt.show()
 
-    # By: smairs
-    def find_lines(self,threshold,noise_range=[3.68,3.73],loglam_range=[3.60,3.96],display_lines=False):
-        '''
-        Find lines with peaks above a specified threshold (in emission only!)
+###########
+# Quick statistical methods/properties: By: mgrawlings1
+###########
 
-        threshold    = Given in terms of multiples of the noise, how high the peak must be to be considered a line
-
-        KWARGS:
-
-        noise_range  = The range within which to measure the noise
-        loglam_range = The range within which to look for interesting lines
-        display_lines= Show a graphical representation of where the lines are
-
-
-        Out:
-
-        Locations of >=5*sigma detections
-
-        Use:
-
-        line_locations, peak_flux = find_lines(datafile)
-        '''
-
-        # First, get the data:
-        loglam, flux = self.loglam, self.flux
-
-        # Next, trim the edges:
-        good_ind     =  np.where(np.logical_and(loglam>=loglam_range[0],loglam<=loglam_range[1]))
-        loglam_good  = loglam[good_ind]
-        flux_good    = flux[good_ind]
-
-        # Now measure the noise:
-        noise        = np.std(flux_good[np.where(np.logical_and(loglam_good>=noise_range[0],loglam_good<=noise_range[1]))],ddof=1)
-        mean         = np.mean(flux_good[np.where(np.logical_and(loglam_good>=noise_range[0],loglam_good<=noise_range[1]))])
-
-        peak_cutoff  = mean+(noise*threshold)
-
-        # Now search for lines!
-        line_locations     = []
-        peak_fluxes        = []
-        line_counter       = []
-        line_counter_dummy = 0
-        flux_dummy         = 0
-        for eachflux,eachloglam in zip(flux_good,loglam_good):
-            if eachflux>=peak_cutoff:
-                if flux_dummy > 0 and flux_dummy < len(flux_good)-2:
-                    if eachflux > flux_good[flux_dummy+1] and eachflux > flux_good[flux_dummy-1]:
-                        line_counter_dummy+=1
-                        line_counter.append(line_counter_dummy)
-                        line_locations.append(eachloglam)
-                        peak_fluxes.append(eachflux)
-            flux_dummy += 1
-
-        # Plot the spectrum with the line locations annotated
-        if display_lines == True:
-            plt.plot(loglam_good,flux_good)
-            for eachline,eachloglam,eachpeakflux in zip(line_counter,line_locations,peak_fluxes):
-                plt.annotate(str(eachline),(eachloglam,eachpeakflux))
-            plt.xlabel('loglam')
-            plt.ylabel('Flux')
-            plt.show()
-
-        return(line_locations,peak_fluxes)        
-
-    def show_lines(self,thresh):
-        '''
-        Show the lines above some SNR threshold
-        This does not include noisy edges
-        '''
-        line_locations,peak_fluxes = self.find_lines(thresh)
-        plt.plot(self.loglam,self.flux)
-        line_number = 0
-        for eachloglam,eachflux in zip(line_locations,peak_fluxes):
-            line_number += 1
-            plt.annotate(str(line_number),(eachloglam,eachflux))
-        plt.xlabel('Loglam')
-        plt.ylabel('Flux')
-        plt.show()
-
-# Statistical functions go here: By: mgrawlings1
     @property
     def fluxav(self):
         return np.average(self.flux)
-
-# We probably shouldn't allow them to set the average themselves?
-#        @fluxav.setter
-#        def fluxav(self,new_fluxav):
-#            self.fluxav = new_fluxav 
     
     @property
     def fluxstd(self):
@@ -160,24 +94,158 @@ class Spectrum():
         chanwidth = (slef.loglam[-1]-self.loglam[0])/self.numpix
         return chanwidth
 
+    # By: izumizuno
+    # get SD in a given loglam range
+    def get_std(self, l_min ,l_max):
+
+        data = np.array([ x for x in zip(self.loglam, self.flux)], dtype=[('loglam', self.loglam.dtype), ('flux', self.flux.dtype)])
+        #l_min=1
+        #l_max=4
+
+        return(np.std(data['flux'][(data['loglam'] > l_min) & (data['loglam'] < l_max)]))
+
+############
+# Methods to Summarise Header Information
+############
+
     # By: smairs
-    # Gaussian fit a region!
+    def weathersum(self):
+        '''
+        Print a Report summarising the weather
+
+        Spectrum.weathersum()
+        '''
+        print('\nSummary of the Weather:')
+        print('-----------------------\n')
+        print(f"Bolometric Pressure = {self.pressure}")
+        print(f"Wind Speed          = {self.windspeed}")
+        print(f"Wind Direction      = {self.winddir}")
+        print(f"Air Temperature     = {self.airtemp}")
+        print(f"Humidity            = {self.humidity}\n")
+
+#############
+# Methods to Find and analyse lines
+#############
+
+    # By: smairs
+    # Find lines above a given threshold in the
+    # dumbest way possible
+    def find_lines(self,threshold,noise_range=[3.68,3.73],loglam_range=[3.60,3.96],display_lines=False):
+        '''
+        Find lines with peaks above a specified threshold (in emission only!)
+
+        threshold    = Given in terms of multiples of the noise (integer), 
+                       how bright the peak must be to be considered a line
+
+        KWARGS:
+
+        noise_range  = The loglam range within which to measure the noise (list, int or float)
+        loglam_range = The loglam range within which to look for interesting lines (list, int or float)
+        display_lines= Show a graphical representation of where the lines are (Bool)
+
+        Out:
+
+        Locations of >=threshold detections
+
+        Use:
+
+        line_locations, peak_flux = Spectrum.find_lines(threshold)
+        '''
+
+        # First, get the data:
+        loglam, flux = self.loglam, self.flux
+
+        # Next, trim the edges:
+        good_ind     = (loglam >= loglam_range[0]) & \
+                       (loglam <= loglam_range[1])
+        loglam_good  = loglam[good_ind]
+        flux_good    = flux[good_ind]
+
+        # Now measure the noise and get the mean flux in that range:
+        noise_range_index = (loglam_good >= noise_range[0]) & \
+                            (loglam_good <= noise_range[1])
+
+        noise        = np.std(flux_good[noise_range_index],ddof=1)
+        mean         = np.mean(flux_good[noise_range_index])
+
+        # Define the peak_cutoff based on the noise
+        # and user supplied threshold
+        peak_cutoff  = mean+(noise*threshold)
+
+        # Now search for lines! 
+        # Just do the dumbest thing possible
+        line_locations     = []
+        peak_fluxes        = []
+        line_counter       = []
+        line_counter_dummy = 0
+        flux_dummy         = 0
+        for eachflux,eachloglam in zip(flux_good,loglam_good):
+            if eachflux >= peak_cutoff:
+                if flux_dummy > 0 and flux_dummy < len(flux_good)-2:
+                    if eachflux > flux_good[flux_dummy+1] and eachflux > flux_good[flux_dummy-1]:
+                        line_counter_dummy+=1
+                        line_counter.append(line_counter_dummy)
+                        line_locations.append(eachloglam)
+                        peak_fluxes.append(eachflux)
+            flux_dummy += 1
+
+        # Plot the spectrum with the line locations annotated
+        if display_lines == True:
+            plt.plot(loglam_good,flux_good)
+            for eachline,eachloglam,eachpeakflux in zip(line_counter,line_locations,peak_fluxes):
+                plt.annotate(str(eachline),(eachloglam,eachpeakflux))
+            plt.xlabel('loglam')
+            plt.ylabel('Flux')
+            plt.show()
+
+        return(line_locations,peak_fluxes)        
+
+    # By: smairs
+    # Show lines detected above a gien threshold
+    def show_lines(self,thresh):
+        '''
+        Show the lines above some SNR threshold
+        This does not include noisy edges
+        '''
+        line_locations,peak_fluxes = self.find_lines(thresh)
+        plt.plot(self.loglam,self.flux)
+        line_number = 0
+        for eachloglam,eachflux in zip(line_locations,peak_fluxes):
+            line_number += 1
+            plt.annotate(str(line_number),(eachloglam,eachflux))
+        plt.xlabel('Loglam')
+        plt.ylabel('Flux')
+        plt.show()
+
+    # By: smairs
+    # Gaussian fit a region of the data!
     def gaussfit(self,loglam_range=[3.74,3.755],display=True):
         '''
         Fit a gaussian to a range of data
 
         amp,cen_lam,sigma,pcov = gaussfit()
         '''
-        loglam_in_range = self.loglam[np.where(np.logical_and(self.loglam >= loglam_range[0],self.loglam <= loglam_range[1]))]
-        flux_in_range   = self.flux[np.where(np.logical_and(self.loglam >= loglam_range[0],self.loglam <= loglam_range[1]))]
+        # Ignore everything outside the specified range
+        range_ind = (self.loglam >= loglam_range[0]) & \
+                    (self.loglam <= loglam_range[1])
+
+
+        loglam_in_range = self.loglam[range_ind]
+        flux_in_range   = self.flux[range_ind]
+
+        # Perform the fit
         n     = len(loglam_in_range)                   #the number of data
         mean  = sum(loglam_in_range*flux_in_range)/n              
         sigma = sum(flux_in_range*(loglam_in_range-mean)**2)/n
+
+        # Define a quick Guassian Function
         def gaus(x,a,x0,sigma):
             return a*np.exp(-(x-x0)**2/(2*sigma**2))
 
         popt,pcov = curve_fit(gaus,loglam_in_range,flux_in_range,p0=[1,mean,sigma])
 
+        # Turn the output into more useful variable names
+        # For return
         amp,cen_lam,sigma = popt[0],popt[1],popt[2]
 
         if display == True:
@@ -190,14 +258,3 @@ class Spectrum():
             plt.show()
 
         return(amp,cen_lam,sigma,pcov)
-    
-    # By: izumizuno
-    def get_std(self, l_min ,l_max):
-
-        data = np.array([ x for x in zip(self.loglam, self.flux)], dtype=[('loglam', self.loglam.dtype), ('flux', self.flux.dtype)])
-        #l_min=1
-        #l_max=4
-
-        return(np.std(data['flux'][(data['loglam'] > l_min) & (data['loglam'] < l_max)]))
-
-    
