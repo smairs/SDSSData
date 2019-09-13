7a82a06ebe7ad5faf6adaf35bc31fba07cd4014e
smairs <s.mairs@eaobservatory.org>
2019-09-13 10:57:15 -1000

    brought over an example SDSS spectrum and began the test_spectrum file

---
+++ SDSSData/test/test_spectrum.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# This file is for the purposes of testing the
+# spectrum class file - ensuring each function works
+# as expected
+
+import sys
+#Ensure we are able to find spectrum.py
+sys.path.append('../')
+
+from spectrum import Spectrum
+
+
+
+
